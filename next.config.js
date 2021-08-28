8099226a9f8df1c678774a33accc69c2781c0eb9
ElibertoSubias <eliberto.subias.ortega@gmail.com>
2021-08-27 19:39:38 -0600

    Se agrega sevdidores de produccion.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    backendURL: 'http://localhost:4000',
-    frontendURL: 'http://localhost:3000'
+    backendURL: 'https://obscure-inlet-81996.herokuapp.com/',
+    frontendURL: 'https://nodesend-cliente-buqdh82mv-elibertosubias.vercel.app/'
   }
+  // backendURL: 'http://localhost:4000',
+  // frontendURL: 'http://localhost:3000'
 }
